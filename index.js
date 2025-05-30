cc3ce8c04d95bd826ecb7e347e594b5fc349b464
karthikeyabommireddy <karthikeya1111reddy@gmail.com>
2025-05-30 20:44:31 +0530

    second commit for dental landing page

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-$(".bottom-sec-3-feature-item").click(function() {
-  if ($(this).hasClass("no-action")) return;
+  const bottomSecItems = document.querySelectorAll(".bottom-sec-3-item:not(.bottom-sec-3-disabled)");
+  const bottomSecImage = document.getElementById("bottom-sec-3-displayImage");
+  const bottomSecImages = ["./img/change1.png", "./img/change2.png", "./img/change3.png"];
+  let bottomSecActive = null;
 
-  $(".bottom-sec-3-feature-item").removeClass("active");
-  $(".bottom-sec-3-toggle-btn").text("+");
+  bottomSecItems.forEach(item => {
+    item.addEventListener("click", () => {
+      const index = parseInt(item.dataset.index);
 
-  $(this).addClass("active");
-  $(this).find(".bottom-sec-3-toggle-btn").text("-");
-
-  var imgSrc = $(this).attr("data-img");
-  var descText = $(this).attr("data-desc");
-
-  $(".bottom-sec-3-img").attr("src", "img/" + imgSrc);
-  $(".bottom-sec-3-desc").text(descText);
-});
+      if (bottomSecActive === index) {
+        item.querySelector(".bottom-sec-3-content").style.display = "none";
+        item.querySelector(".bottom-sec-3-symbol").textContent = "+";
+        bottomSecActive = null;
+      } else {
+        if (bottomSecActive !== null) {
+          const prev = document.querySelector(`.bottom-sec-3-item[data-index='${bottomSecActive}']`);
+          prev.querySelector(".bottom-sec-3-content").style.display = "none";
+          prev.querySelector(".bottom-sec-3-symbol").textContent = "+";
+        }
+        item.querySelector(".bottom-sec-3-content").style.display = "block";
+        item.querySelector(".bottom-sec-3-symbol").textContent = "−";
+        bottomSecImage.src = bottomSecImages[index];
+        bottomSecActive = index;
+      }
+    });
+  });
